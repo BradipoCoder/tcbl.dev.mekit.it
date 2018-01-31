94284da0412a6dc1ba9511e3cc12807d3dc2cbad
koalacoder <luca.cattaneo@mekit.it>
2018-01-31 15:01:20 +0100

    Revert "tcbl feed - gathering data"
    This reverts commit fb7a639a798ff23b358a92e19022cb11d8ecc190.

--- sites/tcbl.eu/modules/tcbl_feed/TcblFeed/FeedFactory.php
+++ sites/tcbl.eu/modules/tcbl_feed/TcblFeed/FeedFactory.php
@@ -20,18 +20,7 @@ class FeedFactory {
   protected static $feeds_cache_file_uri = 'public://tcbl_feeds/feeds.ser';
 
 
-  /**
-   * @param array $options
-   */
-  public static function generateFeeds($options = [])
-  {
-    if(isset($options["fake_feeds"]["generate"]) && $options["fake_feeds"]["generate"])
-    {
-      FeedFactory::generateFakeFeeds($options);
-    } else {
-      FeedFactory::generateFeedsFromExternalSources($options);
-    }
-  }
+
 
   /**
    * @param array $options
@@ -60,31 +49,7 @@ class FeedFactory {
   {
     $feeds = [];
 
-    $fileRealPath = drupal_realpath(FeedFactory::$feeds_cache_file_uri);
-    if($fileRealPath && file_exists($fileRealPath))
-    {
-      $flatData = file_get_contents($fileRealPath);
-      if($flatData && !empty($flatData))
-      {
-        $feeds = unserialize($flatData);
-      }
-    }
-
     return $feeds;
-  }
-
-  /**
-   * Generate feed elements using plugins to read external sources
-   *
-   * @param array $options
-   *
-   */
-  protected static function generateFeedsFromExternalSources($options = [])
-  {
-    //@TBW
-    $feeds = [];
-
-    FeedFactory::writeFeedsFile($feeds);
   }
 
   /**
@@ -103,25 +68,17 @@ class FeedFactory {
       $item = new FeedItem();
       $item->setType("facebook");
       $item->setTitle("Item #".$i);
-      $item->setMessage("something...");
-      $item->setCreationDate(new \DateTime());
+      $item->setContent("something...");
+      $item->setDate(new \DateTime());
       $item->setUrl("https://mekit.it");
       array_push($feeds, $item);
     }
 
-    FeedFactory::writeFeedsFile($feeds);
-  }
-
-  /**
-   * Serializes and writes out the feeds
-   *
-   * @param $feeds
-   */
-  protected static function writeFeedsFile($feeds)
-  {
+    //write file to FS
     $flatData = serialize($feeds);
     $feedDir = dirname(FeedFactory::$feeds_cache_file_uri);
     file_prepare_directory($feedDir, FILE_CREATE_DIRECTORY&&FILE_MODIFY_PERMISSIONS);
     file_save_data($flatData, FeedFactory::$feeds_cache_file_uri,FILE_EXISTS_REPLACE);
   }
+
 }
