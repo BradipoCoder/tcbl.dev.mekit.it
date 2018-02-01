25a9e2efe94042938e28991ae2606cdb9d773377
koalacoder <luca.cattaneo@mekit.it>
2018-02-01 15:40:41 +0100

    Template (clean template)

--- sites/tcbl.eu/themes/tcbl/templates/page/node--page--full.tpl.php
+++ sites/tcbl.eu/themes/tcbl/templates/page/node--page--full.tpl.php
@@ -21,26 +21,8 @@
   <div class="node-content"<?php print $content_attributes; ?>>
     <div class="row">
       <div class="container">
-        <?php print render($content['title_field']); ?>
-        <?php print render($content['field_short']); ?>
-        <?php print render($content['field_img']); ?>
-
-        <?php print render($content['cat_menu']); ?>
-        <?php print render($content['posts']); ?>
-
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-            <?php print render($content); ?>
-          </div>
-        </div>
-
-        <?php print render($content['webform']); ?>
-        <?php print render($content['list']); ?>
-        <?php print render($content['children']); ?>
-        <?php print render($content['press']); ?>
+        <?php print render($content); ?>
       </div>
     </div>
   </div>
 </div>
-
-<?php print render($content['btm_form']); ?>
