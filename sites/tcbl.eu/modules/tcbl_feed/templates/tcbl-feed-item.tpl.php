70d79601c2c9bbff340f3570cdb3e9b74df7868a
jakabadambalazs <jakabadambalazs@gmail.com>
2018-03-02 14:43:49 +0100

    Feed module - theming #1

--- sites/tcbl.eu/modules/tcbl_feed/templates/tcbl-feed-item.tpl.php
+++ sites/tcbl.eu/modules/tcbl_feed/templates/tcbl-feed-item.tpl.php
@@ -1,9 +1,8 @@
 <?php
 /** @var \TcblFeed\FeedItem $feed_item */
-
-/*  <pre><?php print_r($feed_item); ?></pre>  */
+/** @var string $classes */
 ?>
-<div class="feed-item col-sm-4">
+<div class="<?php print $classes; ?> col-sm-4">
   <div class="well">
     <h5><?php print $feed_item->getTitle(); ?></h5>
     <code>TYPE: <?php print $feed_item->getType(); ?></code>
---
+++ sites/tcbl.eu/themes/tcbl/templates/tcbl_feed_item/tcbl-feed-item.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/** @var \TcblFeed\FeedItem $feed_item */
+/** @var string $classes */
+
+//print_r(get_defined_vars());
+
+/*  <pre><?php print_r($feed_item); ?></pre>  */
+?>
+<div class="<?php print $classes; ?> col-sm-4">
+  <div class="well">
+    <h5><?php print $feed_item->getTitle(); ?></h5>
+    <code><?php print $feed_item->getSource(); ?>/<?php print $feed_item->getType(); ?></code>
+  </div>
+</div>
+
