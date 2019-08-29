b0875450c8d750f6248b7f00a2f956e142d34013
Luca Cattaneo <luca.cattaneo@mekit.it>
2019-08-29 15:44:07 +0200

    Short Runs - progress and cookies

--- sites/tcbl.eu/modules/tcbl_sruns/js/tcbl-sruns.js
+++ sites/tcbl.eu/modules/tcbl_sruns/js/tcbl-sruns.js
@@ -5,6 +5,20 @@
 (function ($, Drupal) {
   Drupal.behaviors.tcblSruns = {
 
+    domLiTopics: false,
+    domQuestions: false,
+    domResults: false,
+    domProgress: false,
+    domNext: false,
+
+    topicsCount: false,
+
+    data: {
+      topics: {},
+      current: false,
+      progress: 0,
+    },
+
     attach: function (context, settings) {
       var me = this;
       // me.archiveId = Drupal.settings.tcbl_comps.id;
@@ -21,63 +35,168 @@
     
     setUp: function(mode){
       if (mode == 'firstTime'){
+        this.setUpVars();
+        this.loadData();
+        this.updateDom();
+
         this.armTopic();
-        this.armYes();
-        this.armNo();
-        this.armSkip();
+        this.armButtons();
       }
+    },
+
+    setUpVars: function(){
+      this.domLiTopics = $('.li-topic');
+      this.domQuestions = $('.question');
+      this.domResults = $('.results');
+      this.domProgress = $('#sruns-progress');
+
+      var current = $('.li-topic.open');
+      var currentTid = current.attr('data-tid');
+      this.data.current = currentTid;
+
+      this.topicsCount = this.domLiTopics.length;
     },
 
     // ** ARM **
     // ---------
     
     armTopic: function(){
-      $('.li-topic').click(function(){
+      var me = this;
+      me.domLiTopics.click(function(){
         var tid = $(this).attr('data-tid');
-        
-        $('.li-topic').removeClass('open');
-        $('.question').addClass('hide');
-        $('.results').addClass('hide');
-
-        // Topic active
-        var li = $('#li-topic-' + tid);
-        li.addClass('open');
-
-        var question = $('#question-' + tid);
-        question.removeClass('hide');
+        me.data.current = tid;
+        me.renderTopic(tid);
+        me.saveData();
       });  
     },
 
-    armYes: function(){
-      $('.bts-yes').click(function(){
-        var tid = $(this).attr('data-tid');
-        var li = $('#li-topic-' + tid);
-        li.addClass('done').removeClass('skip');
-      })
+    armButtons: function(){
+      var me = this;
+      $('.bts').click(function(){
+        var item = $(this);
+        var tid = item.attr('data-tid');
+        var value = false;
+
+        if (item.hasClass('bts-yes')){
+          value = 'yes';
+        }
+        if (item.hasClass('bts-no')){
+          value = 'no';
+        }
+        if (item.hasClass('bts-skip')){
+          value = 'skip';
+        }
+
+        me.data.topics[tid] = value;
+        me.renderAnswer(tid, value);
+        me.updateProgress();
+        me.renderProgress();
+        me.saveData();
+      });
     },
 
-    armNo: function(){
-      $('.bts-no').click(function(){
-        $('.results').addClass('hide');
-        var tid = $(this).attr('data-tid');
-        var li = $('#li-topic-' + tid);
-        li.removeClass('done').removeClass('skip');
-        var result = $('#result-' + tid);
+    // ** RENDER **
+    // ------------
+    
+    renderAnswer: function(tid, value){
+      var me = this;
+      var wrapper = me.getDomItemFromId('wrapper-bts', tid); 
+      var btns = $('.bts', wrapper);
+      btns.removeClass('checked');
+
+      var btn = me.getDomItemFromId('bts-' + value, tid);
+      btn.addClass('checked');
+
+      var li = me.getDomItemFromId('li-topic', tid);
+      li.removeClass('answ-no').removeClass('answ-yes').removeClass('answ-skip');
+      li.addClass('done').addClass('answ-' + value);
+
+      var result = me.getDomItemFromId('result', tid);
+
+      // Yes
+      if (value == 'yes'){
+        li.removeClass('skip');
+        result.addClass('hide');
+      }
+
+      // No
+      if (value == 'no'){
+        li.removeClass('skip');
+        me.domResults.addClass('hide');
         result.removeClass('hide');
         $('.sameh', result).sameh();
-      })
+      }
+
+      // Skip
+      if (value == 'skip'){
+        li.addClass('skip');
+        result.addClass('hide');
+      }
     },
 
-    armSkip: function(){
-      $('.bts-skip').click(function(){
-        var tid = $(this).attr('data-tid');
-        var li = $('#li-topic-' + tid);
-        li.addClass('done').addClass('skip');  
-      });
+    renderTopic: function(tid){
+      this.domLiTopics.removeClass('open');
+      this.domQuestions.addClass('hide').css('style', '');
+      this.domResults.addClass('hide');
+
+      // Topic active
+      var li = this.getDomItemFromId('li-topic', tid);
+      li.addClass('open');
+
+      var question = this.getDomItemFromId('question', tid);
+      question.hide().removeClass('hide').fadeIn();  
+    },
+
+    renderProgress: function(){
+      console.debug('Render progress: ' + this.data.progress);
+      $('.s-progress', this.domProgress).css('width', this.data.progress + '%');
+
+      if (this.data.progress == 100){
+        $('.btn', this.domNext).removeClass('disabled').removeClass('btn-default').addClass('btn-success');  
+      } else {
+        $('.btn', this.domNext).addClass('disabled').removeClass('btn-success').addClass('btn-default'); 
+      }
+    },
+
+    // ** SAVE **
+    // ----------
+    
+    saveData: function(){
+      Cookies.set('sruns-data', this.data, { expires: 30 });
+    },
+
+    loadData: function(){
+      var data = Cookies.get('sruns-data');
+      if (data !== undefined){
+        this.data = JSON.parse(data);
+        return true;
+      }
+      return false;
     },
 
     // ** ACTIONS **
     // -------------
+
+    updateDom: function(){
+      var me = this;
+      var topics = this.data.topics;
+      _.each(topics,function(value, tid){
+        me.renderAnswer(tid, value);
+      })
+
+      if (this.data.current){
+        me.renderTopic(this.data.current);
+      }
+
+      me.renderProgress();
+    },
+
+    updateProgress: function(){
+      var yes = this.domLiTopics.filter('.answ-yes').length;
+      var skip = this.domLiTopics.filter('.answ-skip').length;
+      var tot = yes + skip;
+      this.data.progress = Math.round(tot / this.topicsCount * 100);
+    },
 
     /**
      * Scroll to the archive
@@ -85,6 +204,20 @@
     scrollToTop: function(){
       var element = Drupal.settings.tcbl_comps.scrollToElement;
       scrollTo(element);
-    }
+    },
+
+    // ** UTILITY **
+    // -------------
+    
+    getDomItemFromId: function(prefix, id){
+      var item = $('#' + prefix + '-' + id);
+      return item;
+    },
+
+    getDomItemFromAttribute: function(domObject, prefix, attributeName){
+      var id = domObject.attr(attributeName);
+      var item = this.getDomItemFromId(prefix, id);
+      return item;
+    },
   };
 })(jQuery, Drupal);
--- sites/tcbl.eu/modules/tcbl_sruns/tcbl-sruns-filter.tpl.php
+++ sites/tcbl.eu/modules/tcbl_sruns/tcbl-sruns-filter.tpl.php
@@ -5,11 +5,19 @@
         <?php foreach ($data['progress']['topics'] as $tid => $topic) : ?>
           <li id="li-topic-<?php print $tid; ?>" class="li-topic <?php $topic['on'] ? print 'open' : false ?>" data-tid="<?php print $tid; ?>">
             <?php print $topic['name']; ?>
-            <i class="fa fa-check-circle i-check"></i>
+            <i class="fa fa-check-circle i-yes"></i>
+            <i class="fa fa-close i-no"></i>
             <i class="fa fa-ban i-skip"></i>
           </li>
         <?php endforeach; ?>
       </ul>
+      <div id="sruns-progress" class="sruns-progress">
+        <div class="s-progress"></div>
+      </div>
+      <div id="sruns-next" class="sruns-next">
+        <p class="help small">Complete all the steps to go on</p>
+        <a href="#" class="btn btn-default disabled">Next</a>
+      </div>
     </div>
     <div id="sruns-content" class="sruns-content">
       <div id="sruns-questions" class="sruns-questions">
@@ -17,10 +25,28 @@
           <div id="question-<?php print $tid; ?>" class="question <?php $question['on'] ? false : print 'hide' ?>" data-tid="<?php print $tid; ?>">
             <?php print render($question['build']); ?>
 
-            <div class="wrapper-bts">
-              <span href="#" data-tid="<?php print $tid; ?>" class="bts bts--flat bts-yes">Yes</span>
-              <span href="#" data-tid="<?php print $tid; ?>" class="bts bts--out bts-no">No</span>
-              <span href="#" data-tid="<?php print $tid; ?>" class="bts bts--out bts--default bts-skip">Not applicable</span>
+            <div id="wrapper-bts-<?php print $tid; ?>" class="wrapper-bts">
+              <span 
+                href="#"
+                id="bts-yes-<?php print $tid; ?>"
+                data-tid="<?php print $tid; ?>"
+                class="bts bts--flat bts-yes">
+                Yes <i class="fa fa-check i-check"></i>
+              </span>
+              <span
+                href="#"
+                id="bts-no-<?php print $tid; ?>"
+                data-tid="<?php print $tid; ?>"
+                class="bts bts--out bts-no">
+                No <i class="fa fa-check i-check"></i>
+              </span>
+              <span
+                href="#"
+                id="bts-skip-<?php print $tid; ?>"
+                data-tid="<?php print $tid; ?>"
+                class="bts bts--out bts--default bts-skip">
+                Not applicable <i class="fa fa-check i-check"></i>
+              </span>
             </div>
           </div>
         <?php endforeach; ?>
